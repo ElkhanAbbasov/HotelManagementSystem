572d3d0ddff7552981c53abc11707ad55017bb70
Ganbarli <71987179+Ganbarli@users.noreply.github.com>
2024-12-11 21:45:35 +0300

    Update HotelMain.java

--- src/services/HotelMain.java
+++ src/services/HotelMain.java
@@ -3,8 +3,13 @@ package services;
 public class HotelMain {
 
 	public static void main(String[] args) {
-		RoomSystem rs = new RoomSystem();
-
+		String[] arr = {"hello", "salam"};
+		
+		RoomSystem.addReserve(1, true, 1, "Water", false, arr, "OneRoom", "01.01.2006", "05.02.2006", 2, "Party");
+		RoomSystem.addReserve(2, true, 1, "Water", false, arr, "OneRoom", "01.01.2006", "05.01.2006", 2, "Party");
+		System.out.println(RoomSystem.getRooms());
+		System.out.println(RoomSystem.sortByPrice());
+		
 	}
 
 }
