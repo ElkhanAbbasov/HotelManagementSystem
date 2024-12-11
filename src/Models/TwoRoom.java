ef7073f58aa41644b2d271e4c72f81bf0dc5860e
Ganbarli <71987179+Ganbarli@users.noreply.github.com>
2024-12-11 13:35:05 +0300

    Update TwoRoom.java

--- src/Models/TwoRoom.java
+++ src/Models/TwoRoom.java
@@ -2,22 +2,43 @@ package Models;
 
 public class TwoRoom extends Room {
     private boolean extraBed;
-    private String[] bedType;
+    private String bed;
+    private String[] bedType = {"Single", "King", "Queen"};
 
-    public TwoRoom(int roomNumber, boolean isOccupied, double price, int floorNumber, String roomType, String[] members, String[] bedType, String startDate, String endDate) {
+    public TwoRoom(int roomNumber, boolean isOccupied, double price, int floorNumber, String roomType, String[] members, String bed, String startDate, String endDate) {
         super(roomNumber, isOccupied, price, floorNumber, roomType, members);
-        this.bedType = bedType;
+        this.bed = bed;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
     @Override
     public boolean checkAvailability() {
-        return !isOccupied;
+        return !isOccupied();
     }
 
     @Override
     public double calculateFee() {
-        return price + (extraBed ? 50 : 0); // Example logic
-    }
+        if(bed.equalsIgnoreCase(bedType[0])) {
+        	price = (super.findNumberOfDays()) * 100;
+        	return price;
+        }else if(bed.equalsIgnoreCase(bedType[1])) {
+        		price = (super.findNumberOfDays()) * 200;
+        		return price;
+        	}else if(bed.equalsIgnoreCase(bedType[1])) {
+        		price = (super.findNumberOfDays()) * 150;
+        		return price;
+        	}
+        return 0;
+        }
+
+	@Override
+	public boolean discount() {
+		if(super.findNumberOfDays() > 5) {
+			double discount = (super.findNumberOfDays() - 5) * 0.20;
+			price *= discount;
+			return true;
+		}
+		return false;
+	}
 }
